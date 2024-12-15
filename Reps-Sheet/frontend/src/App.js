f11916c131b2c63d91049e166cf7565b502a38cc
Thanujaya Tennekoon <eg245364@engug.ruh.ac.lk>
2024-12-15 09:51:05 +0530

    first commit

--- Reps-Sheet/frontend/src/App.js
+++ Reps-Sheet/frontend/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="./logo192.png" alt="logo" class='logo_m'/>
+        <h1>Reps-Sheet</h1>
+        <h2>Your Daily Workout Diary</h2>
+        <marquee>WEBSIRE IS UNDER CONSTRUCTION!</marquee>
+        
       </header>
     </div>
   );
