7c077d420c2283cad593a9e0a06c45eb506be7c1
Thanujaya Tennekoon <eg245364@engug.ruh.ac.lk>
2025-01-04 20:45:28 +0530

    Online Server restarted

--- Reps-Sheet/backend/server.js
+++ Reps-Sheet/backend/server.js
@@ -11,16 +11,16 @@ app.use(express.json());
 
 // Database connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "reps-sheet"
+    // host: "localhost",
+    // user: "root",
+    // password: "",
+    // database: "reps-sheet"
     
-    // host: "bp2juxysn0nszxvmkkzj-mysql.services.clever-cloud.com",
-    // user: "udflccbdblfustx7",
-    // password: "qgnCvYDdKjXJIfaLe8hL",
-    // database: "bp2juxysn0nszxvmkkzj",
-    // port: 3306
+    host: "bp2juxysn0nszxvmkkzj-mysql.services.clever-cloud.com",
+    user: "udflccbdblfustx7",
+    password: "qgnCvYDdKjXJIfaLe8hL",
+    database: "bp2juxysn0nszxvmkkzj",
+    port: 3306
 });
 
 // Signup endpoint (stores name/email/password in `login`)
